549be6046eb18eeb49fddb46645a5fca7a41dedd
shahadat200 <166094197+shahadat200@users.noreply.github.com>
2025-02-22 21:29:17 +0600

    upadte middleware

---
+++ backend/middleware/handleusers.js
@@ -0,0 +1,28 @@
+const jwt=require('jsonwebtoken')
+const mongoose=require('mongoose')
+const users=require('../model/signUp')
+
+function handleusers(req,res,next){
+
+    const {authorization}=req.headers
+
+    if(!authorization){
+        res.status(401).json({error:"Not Valid"})
+    }else{
+        const token=authorization.replace("Bearer ","")
+        jwt.verify(token,process.env.jwt_secret,(err,payload)=>{
+            if(err){
+                res.status(401).json({error:err})
+            }else{
+                const {_id}=payload
+                users.findById(_id).then(saveUsers=>{
+                    req.user=saveUsers
+                    next()
+                })
+            }
+        })
+    }
+
+}
+
+module.exports=handleusers
