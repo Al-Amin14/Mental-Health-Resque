daf6018cc7f2f796710f8f0036201bb94007e7b4
shahadat200 <166094197+shahadat200@users.noreply.github.com>
2025-03-03 07:25:49 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState, useCallback, useMemo } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/navbar';
-
 import SideBar from './components/SideBar';
 import Home from './screens/home';
 import Activity from './screens/activity';
@@ -13,95 +11,105 @@ import Report from './screens/report';
 import LoginPage from './screens/login';
 import SignUp from './screens/signup';
 import Chat from './screens/chat';
-import ChatList from './screens/chatlist'
-import { ToastContainer } from 'react-toastify'
+import ChatList from './screens/chatlist';
+import { ToastContainer } from 'react-toastify';
 import { loginContext } from './contex/logincontext';
 import NotificationList from './components/notification';
+import io from 'socket.io-client';
 
+const endpoint = "http://localhost:3003";
 
 function App() {
-
   const [loged, setLoged] = useState(false);
   const [slideShow, setSlideShow] = useState(true);
-  const [tochatlist, setTochatlist] = useState(false);
-  const [vlogshome, setVlogshome] = useState(true);
-  const [vlogpost, setVlogpost] = useState(false);
-  const [myposts, setMyposts] = useState(false);
   const [notification, setNotification] = useState([]);
   const [totalchat, setTotalchat] = useState([]);
-  const [notifcounting,setNotifcounting]=useState(0)
-  const [chatiduser, setChatiduser] = useState("");
+  const [notifcounting, setNotifcounting] = useState(0);
+  const [chatiduser, setChatiduser] = useState('');
   const [checkAnother, setCheckAnother] = useState(true);
+  const [ioresult, setIoresult] = useState('');
+  const [socket, setSocket] = useState(null);
+
+  const token = localStorage.getItem('jwt');
+  
+  
+  useEffect(() => {
+    if (token) {
+      setSocket(io(endpoint));
+      setLoged(true);
+    }
+  }, [token]);
 
  
   useEffect(() => {
+    if (socket) {
+      socket.on('newNotification', (notification) => {
+        setNotification((prevNotifications) => [...prevNotifications, notification]);
+        setNotifcounting((prevCount) => prevCount + 1);
+      });
 
-    const token=localStorage.getItem('jwt')
-      if(token){
-        setLoged(true)
-      }
-    
+      socket.on('newMessage', (message) => {
+        setTotalchat((prevChats) => [...prevChats, message]);
+      });
+
+      // Clean up socket event listeners
+      return () => {
+        socket.off('newNotification');
+        socket.off('newMessage');
+      };
+    }
+  }, [socket]);
+
+  const toggleSlideShow = useCallback(() => {
+    setSlideShow((prevState) => !prevState);
   }, []);
 
-  
+  const renderRoutes = useMemo(() => (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Activity" element={<Activity />} />
+      <Route path="/Education" element={<Education />} />
+      <Route path="/Physchologist" element={<Physchologist />} />
+      <Route path="/Report" element={<Report />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="/chatlist" element={<ChatList />} />
+      <Route path="/notifications" element={<NotificationList />} />
+    </Routes>
+  ), []);
 
-  const toggleing=()=>{
-    
-    if(slideShow){
-      setSlideShow(false)
-
-    }else{
-      setSlideShow(true)
+  const ProtectedRoute = ({ children }) => {
+    if (!loged) {
+      return <Navigate to="/login" />;
     }
-  }
+    return children;
+  };
 
   return (
     <BrowserRouter>
       <div>
-
-        <loginContext.Provider value={{checkAnother,setCheckAnother,notifcounting,setNotifcounting,loged,setLoged,totalchat,setTotalchat,tochatlist,setTochatlist,vlogshome,setVlogshome,vlogpost,setVlogpost,myposts,setMyposts,notification,setNotification,chatiduser, setChatiduser}} >
-
-        <Navbar toggleing={toggleing} />
-        <div className='flex w-[100%] h-auto'>
-          {
-            slideShow &&
-          (<div className='w-[20%] bg-slate-200 h-auto flex justify-center items-start'>
-            <SideBar  />
-          </div>)
-          }
-            {slideShow ? (<div className='h-auto bg-gray-100 w-[80%] '> <Routes>
-              <Route path="/" element={<Home/>} ></Route>
-              <Route path="/Activity" element={<Activity/>} ></Route>
-              <Route path="/Education" element={<Education/>} ></Route>
-              <Route path="/Physchologist" element={<Physchologist/>} ></Route>
-              <Route path="/Report" element={<Report/>} ></Route>
-              <Route path='/login' element={<LoginPage/>}> </Route>
-              <Route path='/signup' element={<SignUp/>}></Route>
-              <Route path='/chat' element={<Chat/>}>  </Route>
-              <Route path='/chatlist' element={<ChatList/>} ></Route>
-              <Route path='/notifications' element={<NotificationList/>} ></Route>
-            </Routes>
-          </div>): (<div className='h-auto bg-gray-100 w-[100%] '> <Routes>
-              <Route path="/" element={<Home/>} ></Route>
-              <Route path="/Activity" element={<Activity/>} ></Route>
-              <Route path="/Education" element={<Education/>} ></Route>
-              <Route path="/Physchologist" element={<Physchologist/>} ></Route>
-              <Route path="/Report" element={<Report/>} ></Route>
-              <Route path='/login' element={<LoginPage/>}> </Route>
-              <Route path='/signup' element={<SignUp/>}></Route>
-              <Route path='/chat' element={<Chat/>}>  </Route>
-              <Route path='/chatlist' element={<ChatList/>} ></Route>
-              <Route path='/notifications' element={<NotificationList/>} ></Route>
-              
-            </Routes>
-          </div>)}
-            
-        </div>
-        <ToastContainer theme='dark' position='top-center' autoClose={2000} />
+        <loginContext.Provider value={{
+          socket, setSocket, ioresult, setIoresult, checkAnother, setCheckAnother,
+          notifcounting, setNotifcounting, loged, setLoged, totalchat, setTotalchat,
+          notification, setNotification, chatiduser, setChatiduser
+        }}>
+          <Navbar toggleing={toggleSlideShow} socket={socket} />
+          <div className="flex w-[100%] h-auto">
+            {slideShow && (
+              <div className="w-[20%] bg-slate-200 h-auto flex justify-center items-start">
+                <SideBar />
+              </div>
+            )}
+            <div className={`h-auto bg-gray-100 ${slideShow ? 'w-[80%]' : 'w-[100%]'}`}>
+              {renderRoutes}
+            </div>
+          </div>
+          <ToastContainer theme="dark" position="top-center" autoClose={2000} />
         </loginContext.Provider>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
